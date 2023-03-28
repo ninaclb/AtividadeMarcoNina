03f99cc244fc1f31c590e69712209562831a1b5f
ninaclb <ninacarolinabarater@gmail.com>
2023-03-28 14:23:48 -0300

    add

--- src/screens/FetchNews.jsx
+++ src/screens/FetchNews.jsx
@@ -1,54 +1,40 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react";
 import { View } from "react-native";
 import { Text } from "react-native-paper";
+import { ScrollView } from "react-native-web";
 import { styles } from "../utils/styles";
 
 export default function FetchNews() {
-    function fetchNewsData(){
-        fetch("https://jsonplaceholder.typicode.com/posts") //fetch faz uma requisição
-        .then((response) => response.json()) //transforma em json
-        .then((json) => console.log(json)) //mostra na tela
-        .catch((error) => console.error(error)) //mostra o erro
-    }
-    useEffect(() => { //useEffect é um hook, ele é executado quando o componente é montado
-        fetchNewsData();
-    }, [])//array vazio, ele só executa uma vez
+  const [data, setData] = useState([]); //useState é um hook, ele é usado para armazenar dados
 
+  function fetchNewsData() {
+    fetch("https://jsonplaceholder.typicode.com/posts") //fetch faz uma requisição
+      .then((response) => response.json()) //transforma em json
+      .then((json) => setData(json)) //mostra na tela
+      .catch((error) => console.error(error)); //mostra o erro
+  }
+  useEffect(() => {
+    //useEffect é um hook, ele é executado quando o componente é montado
+    fetchNewsData();
+  }, []); //array vazio, ele só executa uma vez
 
-
-    const persons = [
-        {
-          id: "1",
-          name: "Ernestina Chiquititas",
-        },
-        {
-          id: "2",
-          name: "Pata pobre",
-        },
-        {
-            id: "3",
-            name: "Maria da Boneca",
-          },
-          {
-            id: "4",
-            name: "Mili sem Mãe",
-          },
-          {
-            id: "2",
-            name: "Mosca não toma banho",
-          },
-
-    ];
-
-    return(
-        <View style={styles.container}>
-        {persons.map((person) => {
-          return (
-            <View>
-              <Text style={styles.item}>{person.name}</Text>
-            </View>
-          );
-        })}
+  return (
+    <View style={styles.container}>
+    <ScrollView>
+      <View>
+      <Text>Fetch News</Text>
+      {data.map(
+        (
+          item //map percorre o array
+        ) => (
+          <View key={item.id}>
+            <Text style={{ fontWeight: "bold" }}>{item.title}</Text>
+            <Text>{item.body}</Text>
+          </View>
+        )
+      )}
       </View>
-    );
+    </ScrollView>
+    </View>
+  );
 }
