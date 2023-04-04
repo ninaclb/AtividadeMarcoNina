e561631caee4133f15dc51746b7eea2eadd63f02
ninaclb <ninacarolinabarater@gmail.com>
2023-04-04 14:32:16 -0300

    add

--- src/screens/RickMorty.jsx
+++ src/screens/RickMorty.jsx
@@ -1,17 +1,24 @@
 import { useEffect, useState } from "react";
 import { Dimensions, ScrollView, View } from "react-native";
-import { Card, Paragraph, Title, ToggleButton, Text, Button } from "react-native-paper";
+import {
+  Card,
+  Paragraph,
+  Title,
+  ToggleButton,
+  Text,
+  Button,
+} from "react-native-paper";
 import { styles } from "../utils/styles";
+
 
 export default function RickMorty() {
   const [data, setData] = useState([]);
   const [buscaBt, setBuscaBt] = useState({
-    genero: "",
-    status: ""
-  })
+    gender: "",
+    status: "",
+  });
 
   useEffect(() => {
-
     // buscar, requerer as informações externas
     fetch("https://rickandmortyapi.com/api/character")
       // essas informações chegam via promessa
@@ -22,13 +29,12 @@ export default function RickMorty() {
       // caso ocorra erros eu mostro o erro no console
       .catch((error) => {
         console.log(error);
-      })
-
-  }, [])
+      });
+  }, []);
 
   useEffect(() => {
     // construir a URL da API com base nas opções de busca
-    const apiUrl = `https://rickandmortyapi.com/api/character?${buscaBt.status}${buscaBt.genero}`;
+    const apiUrl = `https://rickandmortyapi.com/api/character?${buscaBt.status}${buscaBt.gender}`;
 
     // buscar, requerer as informações externas
     fetch(apiUrl)
@@ -40,60 +46,53 @@ export default function RickMorty() {
       // caso ocorra erros eu mostro o erro no console
       .catch((error) => {
         console.log(error);
-      })
-  }, [buscaBt.status, buscaBt.genero]);
+      });
+  }, [buscaBt.status, buscaBt.gender]);
 
   const handleStatusFilter = (status) => {
     setBuscaBt({
       ...buscaBt,
-      status: `&status=${status}`
+      status: `&status=${status}`,
     });
   };
 
   const handleGenderFilter = (gender) => {
     setBuscaBt({
       ...buscaBt,
-      gender: `&gender=${gender}`
+      gender: `&gender=${gender}`,
     });
   };
-
 
   return (
     <View style={styles.containerFullWidth}>
       <View>
         <View style={{ flexDirection: "row", alignItems: "center" }}>
           <Text>Status do personagem: </Text>
-          <Button
-            onPress={() => handleStatusFilter("Alive")}
-          >Vivo</Button>
-          <Button
-            onPress={() => handleStatusFilter("Dead")}
-          >Morto</Button>
-          <Button
-            onPress={() => handleStatusFilter("unknown")}
-          >Desconhecido</Button>
+          <Button onPress={() => handleStatusFilter("Alive")}>Vivo</Button>
+          <Button onPress={() => handleStatusFilter("Dead")}>Morto</Button>
+          <Button onPress={() => handleStatusFilter("unknown")}>
+            Desconhecido
+          </Button>
         </View>
         <View style={{ flexDirection: "row", alignItems: "center" }}>
           <Text>Gênero: </Text>
-          <Button
-            onPress={() => handleGenderFilter("Male")}
-          >Masculino</Button>
-          <Button
-            onPress={() => handleGenderFilter("Female")}
-          >Feminino</Button>
-          <Button
-            onPress={() => handleGenderFilter("Genderless")}
-          >Sem Gênero</Button>
-          <Button
-            onPress={() => handleGenderFilter("unknown")}
-
-          >Desconhecido</Button>
+          <Button onPress={() => handleGenderFilter("Male")}>Masculino</Button>
+          <Button onPress={() => handleGenderFilter("Female")}>Feminino</Button>
+          <Button onPress={() => handleGenderFilter("Genderless")}>
+            Sem Gênero
+          </Button>
+          <Button onPress={() => handleGenderFilter("unknown")}>
+            Desconhecido
+          </Button>
         </View>
       </View>
       <ScrollView style={styles.widthFull}>
         <View style={{ ...styles.container, ...styles.widthFull }}>
           {data.map((item) => (
-            <Card style={{ ...styles.widthFull, marginBottom: 20 }} key={item.name}>
+            <Card
+              style={{ ...styles.widthFull, marginBottom: 20 }}
+              key={item.id}
+            >
               <Card.Title>{item.name}</Card.Title>
               <Card.Content>
                 <Title>{item.name}</Title>
@@ -108,6 +107,5 @@ export default function RickMorty() {
         </View>
       </ScrollView>
     </View>
-
   );
 }
